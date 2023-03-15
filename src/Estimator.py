de281c05ffbfc11bc0d4b3f8b14ad5530beb5ade
Fangyu Wu <fangyuwu@berkeley.edu>
2023-03-14 22:24:58 -0700

    Update unicycle_node and expand skeleton code in Estimator.py

--- src/Estimator.py
+++ src/Estimator.py
@@ -1,8 +1,9 @@
 import rospy
 from std_msgs.msg import Float32MultiArray
 import matplotlib.pyplot as plt
+import numpy as np
 plt.rcParams['font.family'] = ['FreeSans', 'Helvetica', 'Arial']
-plt.rcParams['font.size'] = 18
+plt.rcParams['font.size'] = 14
 
 
 class Estimator:
@@ -15,46 +16,86 @@ class Estimator:
 
     Attributes:
     ----------
+        d : float
+            Half of the track width (m) of TurtleBot3 Burger.
+        r : float
+            Wheel radius (m) of the TurtleBot3 Burger.
         u : list
-            A list of system inputs
+            A list of system inputs, where, for the ith data point u[i],
+            u[i][0] is timestamp (s),
+            u[i][1] is left wheel rotational speed (rad/s), and
+            u[i][2] is right wheel rotational speed (rad/s).
         x : list
-            A list of system states
+            A list of system states, where, for the ith data point x[i],
+            x[i][0] is timestamp (s),
+            x[i][1] is bearing (rad),
+            x[i][2] is translational position in x (m),
+            x[i][3] is translational position in y (m),
+            x[i][4] is left wheel rotational position (rad), and
+            x[i][5] is right wheel rotational position (rad).
         y : list
-            A list of system outputs
+            A list of system outputs, where, for the ith data point y[i],
+            y[i][0] is timestamp (s),
+            y[i][1] is translational position in x (m) when freeze_bearing:=true,
+            y[i][1] is distance to the landmark (m) when freeze_bearing:=false,
+            y[i][2] is translational position in y (m) when freeze_bearing:=true, and
+            y[i][2] is relative bearing (rad) w.r.t. the landmark when
+            freeze_bearing:=false.
         x_hat : list
-            A list of estimated system states
+            A list of estimated system states. It should follow the same format
+            as x.
         dt : float
-            Update frequency of the estimator
+            Update frequency of the estimator.
         fig : Figure
-            matplotlib Figure for real-time plotting
-        ax : Axis
-            matplotlib Axis for real-time plotting
-        ln_x : Line
-            matplotlib Line object for ground truth states
-        ln_x_hat : Line
-            matplotlib Line object for estimated states
+            matplotlib Figure for real-time plotting.
+        axd : dict
+            A dictionary of matplotlib Axis for real-time plotting.
+        ln* : Line
+            matplotlib Line object for ground truth states.
+        ln_*_hat : Line
+            matplotlib Line object for estimated states.
         canvas_title : str
-            Title of the real-time plot, which is chosen to be estimator type
+            Title of the real-time plot, which is chosen to be estimator type.
         sub_u : rospy.Subscriber
-            ROS subscriber for system inputs
+            ROS subscriber for system inputs.
         sub_x : rospy.Subscriber
-            ROS subscriber for system states
+            ROS subscriber for system states.
         sub_y : rospy.Subscriber
-            ROS subscriber for system outputs
+            ROS subscriber for system outputs.
         tmr_update : rospy.Timer
-            ROS Timer for periodically updating the estimator's state
+            ROS Timer for periodically invoking the estimator's update method.
+
+    Notes
+    ----------
+        The frozen bearing is pi/4 and the landmark is positioned at (0.5, 0.5).
     """
     # noinspection PyTypeChecker
     def __init__(self):
+        self.d = 0.08
+        self.r = 0.033
         self.u = []
         self.x = []
         self.y = []
         self.x_hat = []  # Your estimates go here!
         self.dt = 0.1
-        self.fig = plt.figure(figsize=(10, 10), dpi=128)
-        self.ax = self.fig.add_subplot(1, 1, 1)
-        self.ln_x, = self.ax.plot([], 'k', linewidth=2, label='x')
-        self.ln_x_hat, = self.ax.plot([], 'o-y', label='x_hat')
+        self.fig, self.axd = plt.subplot_mosaic(
+            [['xy', 'phi'],
+             ['xy', 'x'],
+             ['xy', 'y'],
+             ['xy', 'thl'],
+             ['xy', 'thr']], figsize=(20.0, 10.0))
+        self.ln_xy, = self.axd['xy'].plot([], 'o-g', linewidth=2, label='True')
+        self.ln_xy_hat, = self.axd['xy'].plot([], 'o-c', label='Estimated')
+        self.ln_phi, = self.axd['phi'].plot([], 'o-g', linewidth=2, label='True')
+        self.ln_phi_hat, = self.axd['phi'].plot([], 'o-c', label='Estimated')
+        self.ln_x, = self.axd['x'].plot([], 'o-g', linewidth=2, label='True')
+        self.ln_x_hat, = self.axd['x'].plot([], 'o-c', label='Estimated')
+        self.ln_y, = self.axd['y'].plot([], 'o-g', linewidth=2, label='True')
+        self.ln_y_hat, = self.axd['y'].plot([], 'o-c', label='Estimated')
+        self.ln_thl, = self.axd['thl'].plot([], 'o-g', linewidth=2, label='True')
+        self.ln_thl_hat, = self.axd['thl'].plot([], 'o-c', label='Estimated')
+        self.ln_thr, = self.axd['thr'].plot([], 'o-g', linewidth=2, label='True')
+        self.ln_thr_hat, = self.axd['thr'].plot([], 'o-c', label='Estimated')
         self.canvas_title = 'N/A'
         self.sub_u = rospy.Subscriber('u', Float32MultiArray, self.callback_u)
         self.sub_x = rospy.Subscriber('x', Float32MultiArray, self.callback_x)
@@ -66,6 +107,8 @@ class Estimator:
 
     def callback_x(self, msg):
         self.x.append(msg.data)
+        if len(self.x_hat) == 0:
+            self.x_hat.append(msg.data)
 
     def callback_y(self, msg):
         self.y.append(msg.data)
@@ -74,25 +117,86 @@ class Estimator:
         raise NotImplementedError
 
     def plot_init(self):
-        self.ax.set_title(self.canvas_title)
-        self.ax.set_xlabel('x (m)')
-        self.ax.set_ylabel('y (m)')
-        self.ax.set_aspect('equal', adjustable='box')
+        self.axd['xy'].set_title(self.canvas_title)
+        self.axd['xy'].set_xlabel('x (m)')
+        self.axd['xy'].set_ylabel('y (m)')
+        self.axd['xy'].set_aspect('equal', adjustable='box')
+        self.axd['xy'].legend()
+        self.axd['phi'].set_ylabel('phi (rad)')
+        self.axd['phi'].legend()
+        self.axd['x'].set_ylabel('x (m)')
+        self.axd['x'].legend()
+        self.axd['y'].set_ylabel('y (m)')
+        self.axd['y'].legend()
+        self.axd['thl'].set_ylabel('theta L (rad)')
+        self.axd['thl'].legend()
+        self.axd['thr'].set_ylabel('theta R (rad)')
+        self.axd['thr'].set_xlabel('Time (s)')
+        self.axd['thr'].legend()
         plt.tight_layout()
 
     def plot_update(self, _):
-        self.plot_line(self.ln_x, self.x)
-        self.plot_line(self.ln_x_hat, self.x_hat)
-        self.ax.legend()
-        return self.ln_x, self.ln_x_hat
+        self.plot_xyline(self.ln_xy, self.x)
+        self.plot_xyline(self.ln_xy_hat, self.x_hat)
+        self.plot_philine(self.ln_phi, self.x)
+        self.plot_philine(self.ln_phi_hat, self.x_hat)
+        self.plot_xline(self.ln_x, self.x)
+        self.plot_xline(self.ln_x_hat, self.x_hat)
+        self.plot_yline(self.ln_y, self.x)
+        self.plot_yline(self.ln_y_hat, self.x_hat)
+        self.plot_thlline(self.ln_thl, self.x)
+        self.plot_thlline(self.ln_thl_hat, self.x_hat)
+        self.plot_thrline(self.ln_thr, self.x)
+        self.plot_thrline(self.ln_thr_hat, self.x_hat)
 
-    def plot_line(self, ln, data):
+    def plot_xyline(self, ln, data):
         if len(data):
-            x = [data[1] for data in data]
-            y = [data[2] for data in data]
+            x = [d[2] for d in data]
+            y = [d[3] for d in data]
             ln.set_data(x, y)
-            self.ax.set_xlim([min(x) * 1.05, max(x) * 1.05])
-            self.ax.set_ylim([min(y) * 1.05, max(y) * 1.05])
+            self.resize_lim(self.axd['xy'], x, y)
+
+    def plot_philine(self, ln, data):
+        if len(data):
+            t = [d[0] for d in data]
+            phi = [d[1] for d in data]
+            ln.set_data(t, phi)
+            self.resize_lim(self.axd['phi'], t, phi)
+
+    def plot_xline(self, ln, data):
+        if len(data):
+            t = [d[0] for d in data]
+            x = [d[2] for d in data]
+            ln.set_data(t, x)
+            self.resize_lim(self.axd['x'], t, x)
+
+    def plot_yline(self, ln, data):
+        if len(data):
+            t = [d[0] for d in data]
+            y = [d[3] for d in data]
+            ln.set_data(t, y)
+            self.resize_lim(self.axd['y'], t, y)
+
+    def plot_thlline(self, ln, data):
+        if len(data):
+            t = [d[0] for d in data]
+            thl = [d[4] for d in data]
+            ln.set_data(t, thl)
+            self.resize_lim(self.axd['thl'], t, thl)
+
+    def plot_thrline(self, ln, data):
+        if len(data):
+            t = [d[0] for d in data]
+            thr = [d[5] for d in data]
+            ln.set_data(t, thr)
+            self.resize_lim(self.axd['thr'], t, thr)
+
+    # noinspection PyMethodMayBeStatic
+    def resize_lim(self, ax, x, y):
+        xlim = ax.get_xlim()
+        ax.set_xlim([min(min(x) * 1.05, xlim[0]), max(max(x) * 1.05, xlim[1])])
+        ylim = ax.get_ylim()
+        ax.set_ylim([min(min(y) * 1.05, ylim[0]), max(max(y) * 1.05, ylim[1])])
 
 
 class OracleObserver(Estimator):
@@ -125,8 +229,8 @@ class DeadReckoning(Estimator):
     with the parameters provided to you in the lab doc. After building the
     model, use the provided inputs to estimate system state over time.
 
-    The method should perfectly predict the state evolution if the system is
-    free of noise. You may use this knowledge to test your implementation.
+    The method should closely predict the state evolution if the system is
+    free of noise. You may use this knowledge to verify your implementation.
 
     Example
     ----------
@@ -143,9 +247,10 @@ class DeadReckoning(Estimator):
         self.canvas_title = 'Dead Reckoning'
 
     def update(self, _):
-        # TODO: You implementation goes here!
-        # You may ONLY use self.u and self.x[0] for estimation
-        raise NotImplementedError
+        if len(self.x_hat) > 0 and self.x_hat[-1][0] < self.x[-1][0]:
+            # TODO: Your implementation goes here!
+            # You may ONLY use self.u and self.x[0] for estimation
+            raise NotImplementedError
 
 
 class KalmanFilter(Estimator):
@@ -157,7 +262,10 @@ class KalmanFilter(Estimator):
     model, use the provided inputs and outputs to estimate system state over
     time via the recursive Kalman filter update rule.
 
-    Hint: You may want to reuse your code from DeadReckoning class.
+    Attributes:
+    ----------
+        phid : float
+            Default bearing of the turtlebot fixed at pi / 4.
 
     Example
     ----------
@@ -170,14 +278,21 @@ class KalmanFilter(Estimator):
     def __init__(self):
         super().__init__()
         self.canvas_title = 'Kalman Filter'
+        self.phid = np.pi / 4
+        # TODO: Your implementation goes here!
+        # You may define the A, C, Q, R, and P matrices below.
 
+    # noinspection DuplicatedCode
+    # noinspection PyPep8Naming
     def update(self, _):
-        # TODO: You implementation goes here!
-        # You may use self.u, self.y, and self.x[0] for estimation
-        raise NotImplementedError
+        if len(self.x_hat) > 0 and self.x_hat[-1][0] < self.x[-1][0]:
+            # TODO: Your implementation goes here!
+            # You may use self.u, self.y, and self.x[0] for estimation
+            raise NotImplementedError
 
 
-class ExtendedKalmanFilter(KalmanFilter):
+# noinspection PyPep8Naming
+class ExtendedKalmanFilter(Estimator):
     """Extended Kalman filter estimator.
 
     Your task is to implement the update method of this class using the u
@@ -188,6 +303,13 @@ class ExtendedKalmanFilter(KalmanFilter):
 
     Hint: You may want to reuse your code from DeadReckoning class and
     KalmanFilter class.
+
+    Attributes:
+    ----------
+        landmark : tuple
+            A tuple of the coordinates of the landmark.
+            landmark[0] is the x coordinate.
+            landmark[1] is the y coordinate.
 
     Example
     ----------
@@ -200,8 +322,14 @@ class ExtendedKalmanFilter(KalmanFilter):
     def __init__(self):
         super().__init__()
         self.canvas_title = 'Extended Kalman Filter'
+        self.landmark = (0.5, 0.5)
+        # TODO: Your implementation goes here!
+        # You may define the Q, R, and P matrices below.
 
+    # noinspection DuplicatedCode
     def update(self, _):
-        # TODO: You implementation goes here!
-        # You may use self.u, self.y, and self.x[0] for estimation
-        raise NotImplementedError
+        if len(self.x_hat) > 0 and self.x_hat[-1][0] < self.x[-1][0]:
+            # TODO: Your implementation goes here!
+            # You may use self.u, self.y, and self.x[0] for estimation
+            raise NotImplementedError
+
